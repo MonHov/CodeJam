a08600b89909b31436006ad571a8be80a338dced
brocsky <justin.forrest@gmail.com>
2013-10-19 15:12:36 -0400

    trying to merge

--- CodeJam/CodeJam/scripts/app.js
+++ CodeJam/CodeJam/scripts/app.js
@@ -2,8 +2,16 @@
 requirejs.config({
     "baseUrl": "scripts",
     "shim": {
-        "phaser": { exports: "Phaser" }
+        "phaser": {
+            exports: "Phaser"
+        },
+        'socketio': {
+          exports: 'io'
+        }
     },
+    "paths": {
+        socketio: "lib/socket.io.min"
+    }
 });
 
 requirejs(["main"]);
