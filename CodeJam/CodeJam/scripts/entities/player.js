681cdd7f7b2e310b7aa2bd28f78349585457a4cf
brocsky <justin.forrest@gmail.com>
2013-10-19 19:02:52 -0400

    set anchor

--- CodeJam/CodeJam/scripts/entities/player.js
+++ CodeJam/CodeJam/scripts/entities/player.js
@@ -9,7 +9,7 @@ define(function() {
         this.sprite.body.collideWorldBounds = true;
         this.sprite.width = 32;
         this.sprite.height = 64;
-        this.sprite.anchor.setTo(.5, .5);
+        this.sprite.anchor.setTo(.5, 0);
 
         //network variables
         this.state = "disconnected";
