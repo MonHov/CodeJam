5e88e0425008eb2618424928adfb46401c2fb191
fmarvez <fmarvez@gmail.com>
2013-10-18 23:22:03 -0700

    Base for the plyer object

--- CodeJam/MonHov.CodeJam/app.ts
+++ CodeJam/MonHov.CodeJam/app.ts
@@ -3,6 +3,17 @@
 
 var game = new Phaser.Game(this, 'content', 800, 600, init, create, update);
 
-function init() { };
-function create() { };
+var player: Phaser.Sprite;
+
+function init() { 
+    game.loader.addTextureAtlas('entities', 'assets/textures/entities.png', 'assets/textures/entities.txt');
+    game.loader.load();
+};
+function create() {
+        player = game.createSprite(game.stage.width * .5 - 50, 200, "entities");
+        player.drag.x = 900;
+        player.maxVelocity.x = 250;
+        player.animations.add('idle', ['assets/player.mario.png'], 10, false, false);
+        player.animations.play('idle');
+ };
 function update() { };
--- CodeJam/MonHov.CodeJam/application.js
+++ CodeJam/MonHov.CodeJam/application.js
@@ -5,10 +5,20 @@ var Whatever = (function () {
 ///<reference path="lib/phaser.d.ts"/>
 ///<reference path="Whatever.ts"/>
 var game = new Phaser.Game(this, 'content', 800, 600, init, create, update);
+var player;
 function init() {
+    game.loader.addTextureAtlas('entities', 'assets/textures/entities.png', 'assets/textures/entities.txt');
+    game.loader.load();
 }
 ;
 function create() {
+    player = game.createSprite(game.stage.width * .5 - 50, 200, "entities");
+    player.drag.x = 900;
+    player.maxVelocity.x = 250;
+    player.animations.add('idle', [
+        'assets/player.mario.png'
+    ], 10, false, false);
+    player.animations.play('idle');
 }
 ;
 function update() {
