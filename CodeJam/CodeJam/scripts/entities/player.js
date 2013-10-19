d9bb20923a14550c1fce52b72717304614745328
Thomas Chandler <tchandler@monetate.com>
2013-10-19 17:33:29 -0400

    Moved physics to local players

--- CodeJam/CodeJam/scripts/entities/player.js
+++ CodeJam/CodeJam/scripts/entities/player.js
@@ -8,13 +8,6 @@ define(function() {
         this.sprite.width = 32;
         this.sprite.height = 64;
 
-        this.sprite.body.maxVelocity.x = 250;
-        this.sprite.body.maxVelocity.y = 900;
-        this.sprite.body.drag.x = 900;
-        this.sprite.body.gravity.y = 12;
-        this.sprite.body.bounce.y = .02;
-        this.sprite.anchor.setTo(.5, null);
-
         //network variables
         this.state = "disconnected";
     }
